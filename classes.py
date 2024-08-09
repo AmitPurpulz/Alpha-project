f201662a8f883fcb97889da75a6394a8054aa4c3
AmitPurpulz <amsgulim@gmail.com>
2024-08-09 14:59:59 +0300

    Added another attribute for the Tower class called "attack_type", indicating which enemies the tower will prioritise attacking

--- classes.py
+++ classes.py
@@ -1,8 +1,8 @@
 import copy
 import random
+import sys
 import time
-
-import Game
+import Game_Settings as G
 class Enemy:
     def __init__(self, name, health, speed, money_drop, base_damage, row, column):
         self.name = name
@@ -20,9 +20,9 @@ class Enemy:
         column = int(self.column)
         if (isinstance(map_2d[row][column], Enemy) or map_2d[row][column] == "spawner"):
             pass
-        Distance_From_Base_Vertical = self.row - Game.Rows // 2
+        Distance_From_Base_Vertical = self.row - G.Rows // 2
         while True:
-            Distance_From_Base_Vertical = self.row - Game.Rows // 2
+            Distance_From_Base_Vertical = self.row - G.Rows // 2
             choices = ["right"]
             if Distance_From_Base_Vertical > 0:
                 choices.append("up")
@@ -62,13 +62,13 @@ class Enemy:
 
     def Move(self, map_2d: list):
         #this is a temporary fix to the problem where the base gets deleted by a enemy who is in the same location as the base but doesnt get erased for some reason
-        if (self.row == Game.Rows//2 and self.column == Game.Columns-1):
+        if (self.row == G.Rows//2 and self.column == G.Columns-1):
             print("THIS IS ON BASE")
             map_2d = self.Destroy_Enemy(map_2d)
             map_2d[self.row][self.column] = "base"
             return map_2d
         #end of the temporary fix
-        if Game.num_of_rounds % self.speed*4 == 0:
+        if G.num_of_rounds % self.speed*4 == 0:
             row = self.row
             column = self.column
             if self.OnSpawner:
@@ -92,22 +92,23 @@ class Enemy:
     def Destroy_Enemy(self, map_2d):
         if self.OnSpawner:
             map_2d[self.row][self.column] = "spawner"
-        elif self.row == Game.Rows//2 and self.column == Game.Columns-1:
+        elif self.row == G.Rows//2 and self.column == G.Columns-1:
             map_2d[self.row][self.column] = "base"
         else:
             map_2d[self.row][self.column] = "road"
-        Game.List_Of_Enemies.pop(Game.List_Of_Enemies.index(self))
+        G.List_Of_Enemies.pop(G.List_Of_Enemies.index(self))
         if (self.health > 0):
-            Game.Player_HP = Game.Player_HP - self.base_damage
+            G.Player_HP = G.Player_HP - self.base_damage
         else:
-            Game.enemies_killed = Game.enemies_killed+1
+            G.enemies_killed = G.enemies_killed+1
         return map_2d
 
 class Tower:
-    def __init__(self, damage, firerate, attack_range, price, row, column):
+    def __init__(self, damage, firerate, attack_range, attack_type, price, row, column):
         self.damage = damage
         self.firerate = firerate
         self.attack_range = attack_range
+        self.attack_type = attack_type
         self.price = price
         self.upgrade_1 = False
         self.upgrade_2 = False
@@ -125,49 +126,77 @@ class Tower:
         enemy.health = enemy.health - self.damage
         if enemy.health <= 0:
             game_map = enemy.Destroy_Enemy(map_2d)
-            Game.Player_Money = Game.Player_Money + enemy.money_drop
+            G.Player_Money = G.Player_Money + enemy.money_drop
         return game_map
 
     def Check_Attack(self, map_2d):
         game_map = map_2d
-        if Game.num_of_rounds % (self.firerate*4) == 0:
-            for row in range(min(self.row + self.attack_range, Game.Rows-1), max(self.row - self.attack_range,0), -1):
-                for column in range(min(self.column + self.attack_range, Game.Columns-1), max(self.column - self.attack_range, 0), -1):
-                    if isinstance(map_2d[row][column], Enemy) and map_2d[row][column] in Game.List_Of_Enemies:
-                        game_map = self.Attack_Enemy(map_2d[row][column], map_2d)
-                        return game_map
-
+        if G.num_of_rounds % (self.firerate*4) == 0:
+            if (self.attack_type == "first"):
+                for column in range(min(self.column + self.attack_range, G.Columns - 1),max(self.column - self.attack_range, 0), -1):
+                    for row in range(min(self.row + self.attack_range, G.Rows - 1), max(self.row - self.attack_range, 0),-1):
+                        if isinstance(map_2d[row][column], Enemy) and map_2d[row][column] in G.List_Of_Enemies:
+                            game_map = self.Attack_Enemy(map_2d[row][column], map_2d)
+                            return game_map
+            elif (self.attack_type == "last"):
+                for column in range(max(self.column - self.attack_range, 0),min(self.column + self.attack_range, G.Columns - 1)):
+                    for row in range(max(self.row - self.attack_range, 0),min(self.row + self.attack_range, G.Rows - 1)):
+                        if isinstance(map_2d[row][column], Enemy) and map_2d[row][column] in G.List_Of_Enemies:
+                            game_map = self.Attack_Enemy(map_2d[row][column], map_2d)
+                            return game_map
+            else:
+                Enemies_In_Range = []
+                for column in range(min(self.column + self.attack_range, G.Columns - 1),max(self.column - self.attack_range, 0), -1):
+                    for row in range(min(self.row + self.attack_range, G.Rows - 1), max(self.row - self.attack_range, 0),-1):
+                        if isinstance(map_2d[row][column], Enemy) and map_2d[row][column] in G.List_Of_Enemies:
+                            Enemies_In_Range.append(map_2d[row][column])
+                if (len(Enemies_In_Range) > 0):
+                    strongest_enemy = 0
+                    strongest_enemy_health = 0
+                    weakest_enemy = 0
+                    weakest_enemy_health = sys.maxsize
+                    for enemy in Enemies_In_Range:
+                        if enemy.health > strongest_enemy_health:
+                            strongest_enemy = enemy
+                            strongest_enemy_health = enemy.health
+                        if enemy.health < weakest_enemy_health:
+                            weakest_enemy = enemy
+                            weakest_enemy_health = enemy.health
+                    if (self.attack_type == "strongest"):
+                        game_map = self.Attack_Enemy(strongest_enemy, game_map)
+                    elif (self.attack_type == "weakest"):
+                        game_map = self.Attack_Enemy(weakest_enemy, game_map)
         return game_map
 
     def Upgrade_Tower(self):
         if (not self.upgrade_1):
             self.upgrade_1 = True
             self.damage = self.damage*1.5
-            Game.Player_Money -= self.upgrade_1_cost
+            G.Player_Money -= self.upgrade_1_cost
         elif (not self.upgrade_2):
             self.upgrade_2 = True
             self.damage = (self.damage/1.5)*2
-            Game.Player_Money -= self.upgrade_2_cost
+            G.Player_Money -= self.upgrade_2_cost
 
 class NormalTower(Tower):
     def __init__(self, row, column):
-        super().__init__(damage=1, firerate=1, attack_range=2, price=10, row=row, column=column)
+        super().__init__(damage=1, firerate=1, attack_range=2, attack_type="first" ,price=10, row=row, column=column)
 
 class ShotgunTower(Tower):
     def __init__(self, row, column):
-        super().__init__(damage=3, firerate=2, attack_range=1, price=20, row=row, column=column)
+        super().__init__(damage=3, firerate=2, attack_range=1, attack_type="first", price=20, row=row, column=column)
 
 class MachinegunTower(Tower):
     def __init__(self, row, column):
-        super().__init__(damage=1, firerate=0.5, attack_range=2, price=30, row=row, column=column)
+        super().__init__(damage=1, firerate=0.5, attack_range=2, attack_type="first", price=30, row=row, column=column)
 
 class SniperTower(Tower):
     def __init__(self, row, column):
-        super().__init__(damage=5, firerate=4, attack_range=4, price=40, row=row, column=column)
+        super().__init__(damage=5, firerate=4, attack_range=4, attack_type="first", price=40, row=row, column=column)
 
 class MinigunTower(Tower):
     def __init__(self, row, column):
-        super().__init__(damage=1, firerate=0.25, attack_range=2, price=50, row=row, column=column)
+        super().__init__(damage=1, firerate=0.25, attack_range=2, attack_type="first", price=50, row=row, column=column)
 
 # Example towers
 towers_list = [NormalTower(0, 0), ShotgunTower(0, 0), MachinegunTower(0, 0), SniperTower(0, 0), MinigunTower(0, 0)]
