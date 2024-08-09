b9baabb663ea111d0b73919910de74fc8d5e2f8b
AmitPurpulz <amsgulim@gmail.com>
2024-08-09 10:21:49 +0300

    changed the name of the file

---
+++ Game_Settings.py
@@ -0,0 +1,14 @@
+import time
+
+Player_HP = 10
+Player_Money = 10
+Enemy_Money = 10
+List_Of_Enemies = []
+List_Of_Towers = []
+last_spawn_time = 0
+num_of_rounds = 1
+enemies_killed = 0
+
+Rows = 10
+Columns = 10
+
