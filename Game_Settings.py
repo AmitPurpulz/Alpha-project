ddc2f9279e797a450d7b5f85bf50358c818707e5
AmitPurpulz <amsgulim@gmail.com>
2024-10-23 12:10:00 +0300

    added a dictionary in Game_Settings to translate the game map tiles into numerical values

--- Game_Settings.py
+++ Game_Settings.py
@@ -1,5 +1,3 @@
-import time
-
 Player_HP = 10
 Player_Money = 10
 Enemy_Money = 10
@@ -8,7 +6,11 @@ List_Of_Towers = []
 last_spawn_time = 0
 num_of_rounds = 1
 enemies_killed = 0
-
+difficulty_level = 1
 Rows = 10
 Columns = 10
+Perm_Player_HP = 10
+Perm_Player_Money = 10
+Perm_Enemy_Money = 10
 
+tile_to_value = {"empty":0, "road":1, "spawner":2, "base":3}
